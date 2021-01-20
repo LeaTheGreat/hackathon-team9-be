3907b37356fa331ab38420b6cbfb4c5378bc5a16
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-20 15:18:11 +0200

    deploy

--- src/server.js
+++ src/server.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 /* Middleware */
-app.use(
-    cors({
-        origin: "http://localhost:3000", //  Need to change once we deploy the app
-        credentials: true,
-    })
-);
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000", //  Need to change once we deploy the app
+//         credentials: true,
+//     })
+// );
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -27,13 +27,13 @@ app.use("/api/option", require("./routes/optionRouter"));
 app.use("/api/question", require("./routes/questionRouter"));
 
 app.get("/", (req, res) => {
-    try {
-        res.send("Welcome to team-9 server!");
-    } catch (error) {
-        res.status(404).send(error.message);
-    }
+  try {
+    res.send("Welcome to team-9 server!");
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port http://localhost:${PORT}`);
+  console.log(`Server is listening on port http://localhost:${PORT}`);
 });
