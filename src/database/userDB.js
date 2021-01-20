f22558a09ce5c019f9dc2a993b1288d5f637bc55
Violetta Kegeles <46904779+vkegeles@users.noreply.github.com>
2021-01-20 16:13:55 +0200

    test

--- src/database/userDB.js
+++ src/database/userDB.js
@@ -1,41 +1,40 @@
-const { ObjectId } = require('mongodb');
-const User = require('../models/user');
+const { ObjectId } = require("mongodb");
+const User = require("../models/user");
 
 class userMethods {
-
-    async get(id) {
-        try {
-            const user = await User.findOne({ _id: ObjectId(id) });
-            return user;
-        } catch (e) {
-            return { error: "ID doesn't exist" };
-        }
+  async get(id) {
+    try {
+      const user = await User.findOne({ _id: ObjectId(id) });
+      return user;
+    } catch (e) {
+      return { error: "ID doesn't exist" };
     }
+  }
 
-    async getByEmail(email) {
-        const user = await User.findOne({ email: email });
-        return user;
-    }
+  async getByEmail(email) {
+    const user = await User.findOne({ email: email });
+    return user;
+  }
 
-    async add(user) {
-        const userDB = await User.create({ ...user });
-        return userDB;
-    }
+  async add(user) {
+    const userDB = await User.create({ ...user });
+    return userDB;
+  }
 
-    async updateProfile(id, updatedInfo) {
-        const updatedUserDB = await User.findByIdAndUpdate(
-            { _id: id },
-            { ...updatedInfo },
-            {
-                new: true,
-                omitUndefined: true,
-                useFindAndModify: false,
-                runValidators: true
-            });
-        console.log(updatedUserDB)
-        return updatedUserDB;
-    }
+  async updateProfile(id, updatedInfo) {
+    const updatedUserDB = await User.findByIdAndUpdate(
+      { _id: id },
+      { ...updatedInfo },
+      {
+        new: true,
+        omitUndefined: true,
+        useFindAndModify: false,
+        runValidators: true,
+      }
+    );
+    console.log(updatedUserDB);
+    return updatedUserDB;
+  }
 }
 
-
-module.exports = { userMethods }
+module.exports = { userMethods };
